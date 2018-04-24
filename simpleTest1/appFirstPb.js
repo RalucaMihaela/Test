10096a572f66ae1517b56c8f19f1b47e5ee6d595
Raluca Ionescu <ionescu.m.raluca@gmail.com>
2018-04-24 22:32:40 +0300

    Removed the condition if the array is bigger than 0

--- simpleTest1/appFirstPb.js
+++ simpleTest1/appFirstPb.js
@@ -1,24 +1,19 @@
 module.exports = function (array) {
-
+    
     var sum = 0;
     let notNumber = 0;
 
-    if(array.length > 0) {
-         for (var index = 0; index < array.length; index++){
-                if(parseInt(array[index]) === array[index]){
-                      if(array[index] % 2 === 0){
-                            sum += array[index];
-                      }
-                 } else {
-                            notNumber++;
-                        }
-         }
-     } else { 
-              return 0;
+    for (var index = 0; index < array.length; index++) {
+        if (parseInt(array[index]) === array[index]) {
+            if (array[index] % 2 === 0) {
+                sum += array[index];
             }
-
-     if(notNumber === array.length){
-         return 0;
-     }
-  return sum;
+        } else {
+            notNumber++;
+        }
+    }
+    if (notNumber === array.length) {
+        return 0;
+    }
+    return sum;
 }
