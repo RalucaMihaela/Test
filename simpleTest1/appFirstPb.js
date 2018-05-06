adfa1def7d7debeb12bb548ec3cfc5b9aeeb2c39
Raluca Ionescu <ionescu.m.raluca@gmail.com>
2018-05-06 20:39:34 +0300

    Assume that the array has only integer numbers

--- simpleTest1/appFirstPb.js
+++ simpleTest1/appFirstPb.js
@@ -1,19 +1,12 @@
 module.exports = function (array) {
     
     var sum = 0;
-    let notNumber = 0;
 
-    for (var index = 0; index < array.length; index++) {
-        if (parseInt(array[index]) === array[index]) {
+    for (var index = 0; index < array.length; index++) {        
             if (array[index] % 2 === 0) {
                 sum += array[index];
             }
-        } else {
-            notNumber++;
-        }
-    }
-    if (notNumber === array.length) {
-        return 0;
-    }
+    } 
+    
     return sum;
 }
